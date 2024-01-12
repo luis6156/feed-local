3866bc0c6f8b27c942aca9263e03d54c8360beda
Luis Micu <miculuis1@gmail.com>
2024-01-13 00:35:16 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     './src/**/*.{html,ts}',
-    './libs/ui/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
