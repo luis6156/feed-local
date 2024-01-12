36166cf67e8a2bac07d6e0ae9ba6f9a5977e5bb7
Luis Micu <miculuis1@gmail.com>
2024-01-13 00:30:04 +0200

    added files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     './src/**/*.{html,ts}',
-    './src/libs/ui/**/*.{html,ts}',
+    './libs/ui/**/*.{html,ts}',
   ],
   theme: {
     extend: {},
